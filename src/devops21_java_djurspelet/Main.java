2a4d1baa23e1e863b0e6f429a10e24e4e57c8967
ps74lab <u1@DESKTOP-MDKMMNQ.localdomain>
2021-09-17 23:14:00 +0200

    Added package name.

---
+++ src/devops21_java_djurspelet/Main.java
@@ -0,0 +1,7 @@
+package devops21_java_djurspelet;
+
+public class Main {
+    public static void main(String[] args) {
+        //starta spelet härifrån
+    }
+}
---
+++ src/devops21_java_djurspelet/Player.java
@@ -0,0 +1,8 @@
+package devops21_java_djurspelet;
+
+public class Player {
+	private String mName;
+	private int mCredits;  //at start 1000
+
+	//ArrayList<mAnimal> mAnimals; // at start = empty
+}
