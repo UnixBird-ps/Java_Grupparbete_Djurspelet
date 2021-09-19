f7a821bcc62166326a31bf98a66659abe61d20f2
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-19 20:20:33 +0200

    Made questions the game asks at start work

--- src/devops21_java_djurspelet/Game.java
+++ src/devops21_java_djurspelet/Game.java
@@ -4,21 +4,16 @@ import java.util.function.DoubleToIntFunction;
 
 public class Game {
 
-	private static final int ATSTART_MIN_ROUNDS  = 5;
-	private static final int ATSTART_MAX_ROUNDS  = 30;
 	private static final int ATSTART_MIN_PLAYERS = 2;
 	private static final int ATSTART_MAX_PLAYERS = 4;
+	private static final int ATSTART_MIN_ROUNDS  = 5;
+	private static final int ATSTART_MAX_ROUNDS  = 30;
 	private static final int ATEND_MINMAX_PLAYERS   = 1;
 
-	private static int mNumOfRoundsRequested;   // Anges av användaren
 	private static int mNumOfPlayersRequested;  // Anges av användaren
+	private static int mNumOfRoundsRequested;   // Anges av användaren
 	//private static ArrayList<Player> mPlayers;
 	private static int mRoundsStillToRun;
-
-
-	private static void runMainGameLoop()
-	{
-	}
 
 
 	Game ()
@@ -26,15 +21,25 @@ public class Game {
 		askGameStartVars();
 		// mRoundsStillToRun = ?; // From user input
 
-		runMainGameLoop()
+		runMainGameLoop();
 	}
+
 
 	private void askGameStartVars()
 	{
-		mNumOfRoundsRequested = 0;
 		mNumOfPlayersRequested = 0;
+ 		mNumOfRoundsRequested = 0;
 		mRoundsStillToRun = 0;
-		System.out.println( "Hur många spelare? Ange ett tal mellan " + ATSTART_MIN_ROUNDS + " och " + ATSTART_MAX_ROUNDS + "." );
+
+		System.out.println( "Hur många spelare? Ange ett tal mellan " + ATSTART_MIN_PLAYERS + " och " + ATSTART_MAX_PLAYERS + ":" );
+
+		System.out.println( "Hur många rundor? Ange ett tal mellan " + ATSTART_MIN_ROUNDS + " och " + ATSTART_MAX_ROUNDS + ":" );
 	}
 
+
+	private void runMainGameLoop()
+	{
+		System.out.println( "Main game loop entered." );
+		System.out.println( "Main game loop ended." );
+	}
 }
