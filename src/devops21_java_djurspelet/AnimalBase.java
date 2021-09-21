057b6c954dd2d153ce8dcd8c04b269e7ef660ee2
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-21 17:01:26 +0200

    Added expected lifespan

--- src/devops21_java_djurspelet/AnimalBase.java
+++ src/devops21_java_djurspelet/AnimalBase.java
@@ -2,17 +2,21 @@ package devops21_java_djurspelet;
 
 public abstract class AnimalBase
 {
+	static final int ATSTART_HEALTH = 100;
+
 	private String mName;
 	private int mPriceAtMaxHealth;
 	private int mHealth;
 	private int mAge;
+	private int mExpectedLifeLength;
 
-	public AnimalBase( int pPriceAtMaxHealth )
+	public AnimalBase( int pPriceAtMaxHealth, int pExpectedLifeLength )
 	{
 		mName = "Unnamed";
 		mPriceAtMaxHealth = pPriceAtMaxHealth;
-		mHealth = 100;
+		mHealth = ATSTART_HEALTH;
 		mAge = 0;
+		mExpectedLifeLength = pExpectedLifeLength;
 	}
 
 
