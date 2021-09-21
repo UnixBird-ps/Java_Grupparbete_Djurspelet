fdfe926e2b80b46869edd12cd47c3b4d508fc23b
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-21 18:49:08 +0200

    Replaced english with swedish

--- src/devops21_java_djurspelet/Store.java
+++ src/devops21_java_djurspelet/Store.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Store
 {
 	static final int ATSTART_QUANTITY_PER_ANIMAL = 10;
-	static final int ATSTART_QUANTITY_PER_FOOD   = 1000; // in kilograms
+	static final int ATSTART_QUANTITY_PER_FOOD   = 2000; // in kilograms
 
 	String mName;                          // Initialized in the constructor
 	public ArrayList<AnimalBase> mAnimals; // Sellable to players
@@ -23,8 +23,15 @@ public class Store
 
 		// Data
 		mAnimals.add( new Cat( 1000 ) );
+		mAnimals.add( new Cat( 1000 ) );
+		mAnimals.add( new Cat( 1000 ) );
+		mAnimals.add( new Rabbit( 1000 ) );
+		mAnimals.add( new Rabbit( 1000 ) );
 		mAnimals.add( new Rabbit( 1000 ) );
 		mAnimals.add( new Horse( 5000 ) );
+
+		mFoods.add( new Forage( "Grovfoder", 2, ATSTART_QUANTITY_PER_FOOD ) );
+		mFoods.add( new Carrots( "Morötter", 15, ATSTART_QUANTITY_PER_FOOD ) );
 	}
 
 
@@ -37,17 +44,19 @@ public class Store
 	public void displayInventory()
 	{
 		// Loop through the list av animals available in store
-		System.out.println( "This animals are available:" );
+		System.out.println( "Vi har dessa djur till försäljning :" );
 		for ( AnimalBase a : mAnimals )
 		{
-			System.out.println( "Djurets hälsa: " + a.getHealth() + "   pris: " + a.getPrice() );
+			String lStr = String.format( "Djurets hälsa: %d%%  pris: %d kr", a.getHealth(), a.getPrice() );
+			System.out.println( lStr );
 		}
 
 		// Loop through the list av foods available in store
-		System.out.println( "This foods are available:" );
+		System.out.println( "Vi har dessa djurfoder till försäljning:" );
 		for ( FoodBase f : mFoods )
 		{
-			System.out.println( "Mat för djuren: " + f.getName() + "   pris: " + f.getPrice() + "   : " + f.getQuantity() );
+			String lStr = String.format( "Mat för djuren: %s   pris: %d/Kg    : %d kg", f.getName(), f.getPrice(), f.getQuantity() );
+			System.out.println( lStr );
 		}
 	}
 }
