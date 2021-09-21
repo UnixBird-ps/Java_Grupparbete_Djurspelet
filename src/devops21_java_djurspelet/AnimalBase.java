52425c0ea065d7ab9bac4ba6f7e32091a9a0b49f
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-21 16:37:12 +0200

    Added mName

--- src/devops21_java_djurspelet/AnimalBase.java
+++ src/devops21_java_djurspelet/AnimalBase.java
@@ -2,14 +2,21 @@ package devops21_java_djurspelet;
 
 public abstract class AnimalBase
 {
+	private String mName;
 	private int mPriceAtMaxHealth;
 	private int mHealth;
-	private String mName;
+	private int mAge;
 
 	public AnimalBase( int pPriceAtMaxHealth )
 	{
 		mName = "Unnamed";
 		mPriceAtMaxHealth = pPriceAtMaxHealth;
 		mHealth = 100;
+		mAge = 0;
 	}
+
+
+	String getName() { return mName; }
+	int getHealth() { return mHealth; }
+	int getPrice() { return mHealth * mPriceAtMaxHealth; }
 }
