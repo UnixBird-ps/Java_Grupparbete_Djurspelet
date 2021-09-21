e7522512d15bbe7684bf4fd2636f80d9fdfed706
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-21 16:16:04 +0200

    Added constructor

--- src/devops21_java_djurspelet/AnimalBase.java
+++ src/devops21_java_djurspelet/AnimalBase.java
@@ -1,5 +1,15 @@
 package devops21_java_djurspelet;
 
-public abstract class AnimalBase {
+public abstract class AnimalBase
+{
+	private int mPriceAtMaxHealth;
+	private int mHealth;
+	private String mName;
 
+	public AnimalBase( int pPriceAtMaxHealth )
+	{
+		mName = "Unnamed";
+		mPriceAtMaxHealth = pPriceAtMaxHealth;
+		mHealth = 100;
+	}
 }
