d9cc91914a6a7f65a42a418772d72d9dcc8ea5ff
AnderssonDennis <dennis_a12@hotmail.com>
2021-09-21 11:11:52 +0200

    Update AnimalBase.java

--- src/devops21_java_djurspelet/AnimalBase.java
+++ src/devops21_java_djurspelet/AnimalBase.java
@@ -1,5 +1,27 @@
 package src.devops21_java_djurspelet;
 
-public class AnimalBase {
 
+public abstract class AnimalBase {
+
+	byte health = 100;
+	boolean gender = new Random().nextBoolean();
+
+	public AnimalBase(byte health, boolean gender) {
+
+		this.health = health;
+		this.gender = gender;
+	}
+
+	public byte getHealth() {
+		return health;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public int FeedMe(FoodBase pFood, int pQuantity) {
+
+	}
+	public abstract void speak();
 }
