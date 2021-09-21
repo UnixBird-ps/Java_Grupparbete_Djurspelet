813b8288ce44a294d50b2285f110415f107ebaa8
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-21 20:16:03 +0200

    Minor changes to comments

--- src/devops21_java_djurspelet/Store.java
+++ src/devops21_java_djurspelet/Store.java
@@ -3,25 +3,25 @@ package devops21_java_djurspelet;
 import java.util.ArrayList;
 
 /**
- *
+ *  Holds foods and animals for sale
  */
 public class Store
 {
-	static final int ATSTART_QUANTITY_PER_ANIMAL = 10;
+	static final int ATSTART_QUANTITY_PER_ANIMAL = 5;
 	static final int ATSTART_QUANTITY_PER_FOOD   = 2000; // in kilograms
 
 	String mName;                          // Initialized in the constructor
 	public ArrayList<AnimalBase> mAnimals; // Sellable to players
 	public ArrayList<FoodBase> mFoods;     // Sellable to players
 
-	// Creates store with a name
+	// Creates a store with a name
 	public Store( String pName )
 	{
 		mName = pName;
 		mAnimals = new ArrayList<>();
 		mFoods   = new ArrayList<>();
 
-		// Data
+		// Create data
 		mAnimals.add( new Cat( 1000 ) );
 		mAnimals.add( new Cat( 1000 ) );
 		mAnimals.add( new Cat( 1000 ) );
@@ -29,21 +29,25 @@ public class Store
 		mAnimals.add( new Rabbit( 1000 ) );
 		mAnimals.add( new Rabbit( 1000 ) );
 		mAnimals.add( new Horse( 10000 ) );
-
 		mFoods.add( new Forage( "Grovfoder", 2, ATSTART_QUANTITY_PER_FOOD ) );
 		mFoods.add( new Carrots( "Morötter", 15, ATSTART_QUANTITY_PER_FOOD ) );
 	}
 
 
+	/**
+	*  Greets a player
+	*/
 	void displayGreeting()
 	{
 		System.out.println( "Välkommen till " + mName );
 	}
 
 
+	/**
+	*  Loops through the list av animals and foods available in store
+	*/
 	public void displayInventory()
 	{
-		// Loop through the list av animals available in store
 		// Write to screen kind, health and price of animals
 		System.out.println( "\nVi har dessa djur till försäljning :" );
 		for ( AnimalBase a : mAnimals )
@@ -52,7 +56,6 @@ public class Store
 			System.out.println( lStr );
 		}
 
-		// Loop through the list av foods available in store
 		// Write to screen name of food, price and how much there is left
 		System.out.println( "Vi har dessa djurfoder till försäljning:" );
 		for ( FoodBase f : mFoods )
