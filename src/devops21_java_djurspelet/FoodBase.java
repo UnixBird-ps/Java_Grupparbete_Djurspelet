5f0adf3046a0001adc454fb4685f77143332cb07
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-21 16:36:30 +0200

    Replaced mCost with mPrice, added quantity

--- src/devops21_java_djurspelet/FoodBase.java
+++ src/devops21_java_djurspelet/FoodBase.java
@@ -3,18 +3,24 @@ package devops21_java_djurspelet;
 public abstract class FoodBase
 {
 	private String mName;
-	private int mCost;
+	private int mPrice;
+	private int mQuantity;
 
 
 	/**
 	* Initializes this object
 	*
 	* @param pName
-	* @param pCost  In kilograms
+	* @param pPrice  In kilograms
 	*/
-	public FoodBase( String pName, int pCost )
+	public FoodBase( String pName, int pPrice, int pQuantity )
 	{
 		mName = pName;
-		mCost = pCost;
+		mPrice = pPrice;
+		mQuantity = pQuantity;
 	}
+
+	public String getName() { return mName; }
+	public int getPrice() { return mPrice; }
+	public int getQuantity() { return mQuantity; }
 }
