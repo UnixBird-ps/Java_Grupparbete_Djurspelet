2215fbc0e8d380039e561f0dd15d77642773b83e
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-09-22 13:02:39 +0200

    Added animal gender and methos for returns

--- src/devops21_java_djurspelet/AnimalBase.java
+++ src/devops21_java_djurspelet/AnimalBase.java
@@ -4,13 +4,17 @@ package devops21_java_djurspelet;
 public abstract class AnimalBase // Enforce creation of subclasses
 {
 	static final int ATSTART_HEALTH = 100;
+	static final int GENDER_MALE    = 1;
+	static final int GENDER_FEMALE  = 2;
 
 	private String mName;
 	private String mKind;
+	private int mGender;
 	private int mPriceAtMaxHealth;
 	private int mHealth;
 	private int mAge;
 	private int mExpectedLifeLength;
+
 
 	/**
 	 * @param pKind                What animal
@@ -21,6 +25,7 @@ public abstract class AnimalBase // Enforce creation of subclasses
 	{
 		mName = "Unnamed";
 		mKind = pKind;
+		mGender = ( (int)( Math.random() * 2 ) + 1 ) > 1 ? GENDER_MALE : GENDER_FEMALE;
 		mPriceAtMaxHealth = pPriceAtMaxHealth;
 		mHealth = ATSTART_HEALTH;
 		mAge = 0;
@@ -29,27 +34,42 @@ public abstract class AnimalBase // Enforce creation of subclasses
 
 
 	/**
-	 * @return  This animal's kind
-	 */
+	* @return  This animal's kind
+	*/
 	public String getKind() { return mKind; }
 
+
 	/**
-	 * @return  Animal's name, if set
-	 */
+	* @return
+	*/
+	public int getGender() { return mGender; }
+	public String getGenderStr()
+	{
+		String lGenderStr = switch ( mGender ) {
+			case GENDER_MALE -> "hane";
+			case GENDER_FEMALE -> "hona";
+			default -> "";
+		};
+		return lGenderStr;
+	}
+
+	/**
+	* @return  Animal's name, if set
+	*/
 	public String getName() { return mName; }
 
 	/**
-	 * @return  Animal's health
-	 */
+	* @return  Animal's health
+	*/
 	public int getHealth() { return mHealth; }
 
 	/**
-	 * @return  Computed price, from health and original price
-	 */
+	* @return  Computed price, from health and original price
+	*/
 	public int getPrice() { return mHealth * mPriceAtMaxHealth / 100; }
 
 	/**
-	 * @return  Animal's age
-	 */
+	* @return  Animal's age
+	*/
 	public int getAge() { return mAge; }
 }
