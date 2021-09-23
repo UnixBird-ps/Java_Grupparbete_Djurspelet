f0e7dfb3c8dc2bb931d29db3aa4e1b3c1fc25904
MauroCataldi <90204110+MauroCataldi@users.noreply.github.com>
2021-09-23 18:22:17 +0200

    fixed redundant system out print

--- src/devops21_java_djurspelet/Player.java
+++ src/devops21_java_djurspelet/Player.java
@@ -25,8 +25,7 @@ public class Player {
     public void buyAnimal(AnimalBase pAnimal) {
         boolean lLoop = true;
         while (lLoop){
-            System.out.print("Välj hur många djur du vill köpa: ");
-            int animalAmount = Game.askForValidNumber("",1,1000);
+            int animalAmount = Game.askForValidNumber("Välj hur många djur du vill köpa: ",1,1000);
             if (mCredits >= pAnimal.getPrice()*animalAmount) {
                 mCredits -= pAnimal.getPrice()*animalAmount;
                 for(int i =0; i<animalAmount;i++){
