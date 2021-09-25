3885d25bafba6528d69f8efd4376daad864632f0
AnderssonDennis <dennis_a12@hotmail.com>
2021-09-25 13:56:52 +0200

    feeding method

--- src/devops21_java_djurspelet/Main.java
+++ src/devops21_java_djurspelet/Main.java
@@ -4,10 +4,7 @@ public class Main
 {
 	public static void main( String[] args )
 	{
-		Carrots carrot = new Carrots("newCarrot", 100);
-		double health = carrot.carrotFood("dog");
-		System.out.println(health);
 		//Start the game here
-		//ame mGame = new Game(); // The game runs within this object
+		Game mGame = new Game(); // The game runs within this object
 	}
 }
